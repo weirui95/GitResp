f07a8a443336ee1bae92e3a1f590b39ec0c1ab22
chenweirui <1282451498@qq.com>
2021-08-22 14:33:35 +0800

    添加了文件Student

---
+++ src/com/msb/controller/Student.java
@@ -0,0 +1,11 @@
+package com.msb.controller;
+
+/**
+ * @Auther: cxs
+ * @Date: 2021/8/22 - 08 - 22 - 14:32
+ * @Description: com.msb.controller
+ * @version: 1.0
+ */
+public class Student {
+    private String name;
+}
